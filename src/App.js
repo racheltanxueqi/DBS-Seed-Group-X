28f51e59aa994dd9abad7f173e91d8add4e2911a
Rachel Tan Xue Qi <snowroars@gmail.com>
2021-05-07 10:32:09 +0800

    setup routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
+    // --------- To be removed ---------
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +21,16 @@ function App() {
         </a>
       </header>
     </div>
+    // ---------
+
+    // ===== start react routing here =========
+    // <Router>
+    // <Navbar/>
+    // <Switch>
+    //   <Route path='/' exact component={} />
+    // </Switch>
+    // </Router>
+    // ====================
   );
 }
 
